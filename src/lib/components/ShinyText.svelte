2fe2bdfa6b7474d6f09252a010f4c2521b176fdb
maxli <maxli@hemlock.com>
2025-07-21 16:10:08 -0700

    add some cute shiny text

---
+++ src/lib/components/ShinyText.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	interface ShinyTextProps {
+		text: string;
+		disabled?: boolean;
+		speed?: number;
+		className?: string;
+	}
+	let { text, disabled = false, speed = 5, className = '' }: ShinyTextProps = $props();
+
+	const animationDuration = `${speed}s`;
+</script>
+
+<div
+	class="text-lg bg-[#b5b5b5a4] text-transparent bg-clip-text inline-block {disabled
+		? ''
+		: 'animate-shine'} {className}"
+	style="background-image: linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%);
+    background-size: 200% 100%;
+    -webkit-background-clip: text;
+    animation-duration: {animationDuration};"
+>
+	{text}
+</div>
---
+++ src/routes/components/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import ShinyText from '$lib/components/ShinyText.svelte';
+
+	let text = $state(`This is a shiny text component!`);
+</script>
+
+<div class="p-6 space-y-4 max-w-4xl mx-auto">
+	<h2 class="text-xl font-semibold">Shiny Text Component</h2>
+	<textarea bind:value={text} class="bg-black text-white w-full h-[100px]"></textarea>
+	<ShinyText {text} speed={3} className="text-4xl" />
+</div>
